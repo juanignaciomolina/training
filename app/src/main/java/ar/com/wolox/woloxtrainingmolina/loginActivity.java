17932331def77b8b3a3e65c4ff6942569ccd6ee9
Juan Ignacio Molina <yojuanignaciomln@gmail.com>
2015-03-18 14:49:16 -0300

    Implementada la regla de "todos los campos son requeridos"

--- app/src/main/java/ar/com/wolox/woloxtrainingmolina/loginActivity.java
+++ app/src/main/java/ar/com/wolox/woloxtrainingmolina/loginActivity.java
@@ -1,39 +1,56 @@
 package ar.com.wolox.woloxtrainingmolina;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class loginActivity extends ActionBarActivity {
+public class loginActivity extends Activity implements View.OnClickListener{
+
+    private EditText mMail;
+    private EditText mPassword;
+    private Button mLogIn;
+    private Button mSignUp;
+    private Context mContext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mContext = getApplicationContext();
+
+        mMail = (EditText) findViewById(R.id.et_email);
+        mPassword = (EditText) findViewById(R.id.et_password);
+        mLogIn = (Button) findViewById(R.id.btn_login);
+        mSignUp = (Button) findViewById(R.id.btn_signup);
+
+        mLogIn.setOnClickListener(this);
+        mSignUp.setOnClickListener(this);
+
     }
 
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login: // LogIn button
+                    if(mMail.getText().toString().equals("") && mPassword.getText().toString().equals("")) muestraToast(getString(R.string.login_require_all));
+                break;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
+            case R.id.btn_signup: // Cancel button
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                break;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void muestraToast (String mensaje) {
+        Toast.makeText(mContext, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
 }
