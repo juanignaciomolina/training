8d8526957754e4af28af43b6a62d0dcd0306230f
Juan Ignacio Molina <yojuanignaciomln@gmail.com>
2015-03-18 14:59:25 -0300

    Implementada la regla de "El email debe ser válido"

--- app/src/main/java/ar/com/wolox/woloxtrainingmolina/loginActivity.java
+++ app/src/main/java/ar/com/wolox/woloxtrainingmolina/loginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,11 +39,23 @@ public class loginActivity extends Activity implements View.OnClickListener{
 
     public void onClick(View v) {
         switch (v.getId()) {
+
             case R.id.btn_login: // LogIn button
-                    if(mMail.getText().toString().equals("") && mPassword.getText().toString().equals("")) muestraToast(getString(R.string.login_require_all));
+
+                    //Regla: Todos los campos son requeridos
+                    if (mMail.getText().toString().equals("") && mPassword.getText().toString().equals("")) {
+                        muestraToast(getString(R.string.login_require_all));
+                        return;
+                    }
+                    //Regla: La dirección de email debe ser válida
+                    if (!validaEmail(mMail.getText().toString())) {
+                        mMail.setError(getString(R.string.login_not_valid_email));
+                        return;
+                    }
+
                 break;
 
-            case R.id.btn_signup: // Cancel button
+            case R.id.btn_signup: // SignUp button
 
                 break;
 
@@ -51,6 +64,10 @@ public class loginActivity extends Activity implements View.OnClickListener{
 
     private void muestraToast (String mensaje) {
         Toast.makeText(mContext, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+    private static boolean validaEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
 }
