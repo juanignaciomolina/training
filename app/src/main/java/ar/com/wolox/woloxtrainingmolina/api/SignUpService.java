39c3e20d24d79ecc984448236f15d7f4389dc7ee
Juan Ignacio Molina <juanignaciomln@gmail.com>
2015-03-25 11:45:48 -0300

    Agregado el tipo de request SignUpService

---
+++ app/src/main/java/ar/com/wolox/woloxtrainingmolina/api/SignUpService.java
@@ -0,0 +1,12 @@
+package ar.com.wolox.woloxtrainingmolina.api;
+
+import ar.com.wolox.woloxtrainingmolina.Config;
+import ar.com.wolox.woloxtrainingmolina.entities.User;
+import retrofit.http.Body;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+public interface SignUpService {
+    @POST(Config.PARSE_USERS)
+    void signUp(@Body User user, retrofit.Callback<User> usuarioCallback);
+}
