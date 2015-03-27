5c4845fec8c64710fde6ee8c945cfc74842a6996
Juan Ignacio Molina <juanignaciomln@gmail.com>
2015-03-27 15:07:09 -0300

    Desharcodeo de los nombres de las tabs y refactor del codigo de la MainActivity

--- app/src/main/java/ar/com/wolox/woloxtrainingmolina/activities/MainActivity.java
+++ app/src/main/java/ar/com/wolox/woloxtrainingmolina/activities/MainActivity.java
@@ -4,27 +4,22 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import ar.com.wolox.woloxtrainingmolina.Config;
 import ar.com.wolox.woloxtrainingmolina.R;
 import ar.com.wolox.woloxtrainingmolina.ui.ViewPagerAdapter;
 import ar.com.wolox.woloxtrainingmolina.ui.widget.SlidingTabLayout;
 
 public class MainActivity extends ActionBarActivity {
 
-    // Declaring Your View and Variables
-
     Toolbar mToolbar;
-    ViewPager pager;
-    ViewPagerAdapter adapter;
-    SlidingTabLayout tabs;
-    CharSequence Titles[]={"NEWS","PROFILE"};
-    int mImageResources[]={R.drawable.tab_news_img_selector, R.drawable.tab_profile_img_selector};
-    int Numboftabs =2;
+    ViewPager mPager;
+    ViewPagerAdapter mAdapter;
+    SlidingTabLayout mTabs;
+    int mNumbOfTabs = 2;
+    CharSequence mTitles[] = new CharSequence[mNumbOfTabs];
+    int mImageResources[] = new int[mNumbOfTabs];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +27,43 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         setToolbar();
+        initVars();
+        initTabs();
 
-        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
-        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs, mImageResources);
+    }
 
-        // Assigning ViewPager View and setting the adapter
-        pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
+    private void initVars(){
+        mTitles[0] = getString(R.string.fragment_news_name);
+        mTitles[1] = getString(R.string.fragment_profile_name);
+        mImageResources[0] = R.drawable.tab_news_img_selector;
+        mImageResources[1] = R.drawable.tab_profile_img_selector;
+    }
 
-        // Assiging the Sliding Tab Layout View
-        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
-        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
-        tabs.setCustomTabView(R.layout.tab, R.id.tab_tv, R.id.tab_img);
+    private void initTabs(){
 
-        // Setting Custom Color for the Scroll bar indicator of the Tab View
-        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+        //Se crea el ViewPagerAdapter y se le pasa el fragmentManager, los titulos, imagenes de las tabs y la cantidad de tabs
+        mAdapter =  new ViewPagerAdapter(getSupportFragmentManager(),mTitles, mNumbOfTabs, mImageResources);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        //Tabs con una custom view (tab.xml)
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setDistributeEvenly(true);
+        mTabs.setCustomTabView(R.layout.tab, R.id.tab_tv, R.id.tab_img);
+
+        //Color custom para el scroll de tabs
+        mTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
             public int getIndicatorColor(int position) {
                 return getResources().getColor(R.color.tabsScrollColor);
             }
         });
 
-        // Setting the ViewPager For the SlidingTabsLayout
-        tabs.setViewPager(pager);
-
+        // Se le asigna el ViewPager al SlidingTabLayout
+        mTabs.setViewPager(mPager);
     }
 
     private void setToolbar() {
-        // Set a toolbar to replace the action bar.
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         //getSupportActionBar().setElevation(2);
