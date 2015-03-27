8e6a69d29bfac1c4a5df3af754c039b9316fbd45
Juan Ignacio Molina <juanignaciomln@gmail.com>
2015-03-27 15:13:13 -0300

    Refactor en el ViewPagerAdapter

--- app/src/main/java/ar/com/wolox/woloxtrainingmolina/ui/ViewPagerAdapter.java
+++ app/src/main/java/ar/com/wolox/woloxtrainingmolina/ui/ViewPagerAdapter.java
@@ -9,31 +9,33 @@ import ar.com.wolox.woloxtrainingmolina.fragments.ProfileFragment;
 
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
-    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
-    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+    CharSequence mTitles[];
+    int mNumbOfTabs;
     int mImgResources[];
 
 
-    // Build a Constructor and assign the passed Values to appropriate values in the class
+    // Constructor del adapter con los atributos necesarios
     public ViewPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb, int mImgResources[]) {
         super(fm);
 
-        this.Titles = mTitles;
-        this.NumbOfTabs = mNumbOfTabsumb;
+        this.mTitles = mTitles;
+        this.mNumbOfTabs = mNumbOfTabsumb;
         this.mImgResources = mImgResources;
 
     }
 
-    //This method return the fragment for the every position in the View Pager
+    //Retornamos el fragment correspondiente a la tab que se esta viendo
     @Override
     public Fragment getItem(int position) {
 
-        if(position == 0) // if the position is 0 we are returning the First tab
+        if(position == 0) // position == 0: la tab de la izquierda
         {
             NewsFragment newsFragment = new NewsFragment();
             return newsFragment;
         }
-        else             // As we are having 2 tabs if the position is not 0 it must be 1 so we are returning second tab
+        // position != 0: la tab de la derecha (se podría cambiar esto para permitir más
+        // tabs si llega a ser necesario)
+        else
         {
             ProfileFragment profileFragment = new ProfileFragment();
             return profileFragment;
@@ -42,21 +44,19 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     }
 
-    // This method return the titles for the Tabs in the Tab Strip
-
+    //Devolvemos el titulo o imagen de la tab correspondiente
     @Override
     public CharSequence getPageTitle(int position) {
-        return Titles[position];
+        return mTitles[position];
     }
 
+    //Este método no lleva @Override porque es una customizacion, no se hereda
     public int getPageImage(int position) {
         return mImgResources[position];
     }
 
-    // This method return the Number of tabs for the tabs Strip
-
     @Override
     public int getCount() {
-        return NumbOfTabs;
+        return mNumbOfTabs;
     }
 }
